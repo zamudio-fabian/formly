9febe7ebd38cdfeb49164bf24df3074d8250ba20
Fabian Zamudio <fabian.zamudio.89@gmail.com>
2025-02-06 14:47:35 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,101 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
+import { CommonModule } from '@angular/common';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { HttpClient } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, MatCardModule, MatIconModule, MatButtonModule, CommonModule, FormlyModule, FormlyMaterialModule, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dinamic-form';
+
+  form = new FormGroup({});
+  model: any = { };
+  formularios: any[] = [
+    {
+      id: "v1",
+      name: "Formulario base V1"
+    },
+    {
+      id: "v2",
+      name: "Formulario base mod V2"
+    },
+    {
+      id: "v3",
+      name: "Formulario input custom"
+    },
+    {
+      id: "v4",
+      name: "Formulario style custom"
+    }
+  ];
+  selectedForm = this.formularios[0].id;
+  envios: any[] = [];
+  fields: FormlyFieldConfig[] = [];
+  envioId: any = null;
+
+  constructor(private readonly http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadJsonData(this.selectedForm);
+  }
+
+  loadJsonData(formulario: string): void {
+    this.http.get(`/assets/json/${formulario}.json`).subscribe((response: any) => {
+      this.fields =  response.data as FormlyFieldConfig[];
+      this.resetForm();
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      let form = new FormGroup({});
+      form.disable();
+      if(this.envioId){
+        this.envios[this.envios.findIndex(e => e.id === this.envioId)] = {
+          id: this.envioId,
+          tarea: this.formularios.find(f => f.id === this.selectedForm),
+          fields: JSON.parse(JSON.stringify(this.fields)), // Copia profunda
+          model: JSON.parse(JSON.stringify(this.model)), // Copia profunda
+          form
+        };
+      }else {
+        this.envios.unshift({
+          id: this.envios.length + 1,
+          tarea: this.formularios.find(f => f.id === this.selectedForm),
+          fields: JSON.parse(JSON.stringify(this.fields)), // Copia profunda
+          model: JSON.parse(JSON.stringify(this.model)), // Copia profunda
+          form
+        });
+      }
+      this.resetForm();
+    }
+  }
+
+  editForm(envio: any) {
+    this.envioId = envio.id;
+    this.fields = envio.fields;
+    this.model = envio.model;
+  }
+
+  cancelEdit() {
+    this.resetForm();
+  }
+
+  private resetForm() {
+    this.envioId = null;
+      this.model = {};
+      this.form = new FormGroup({});
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { FormlyModule } from '@ngx-formly/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { MyCustomInputComponent } from './custom-input/my-custom-input.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimations(),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withEventReplay()),
+    importProvidersFrom([
+      FormlyModule.forRoot({
+          types: [
+            {
+              name: 'custom-input',
+              component: MyCustomInputComponent
+            },
+          ],
+      }),
+      FormlyMaterialModule
+    ]), provideAnimationsAsync()
+  ]
 };
+
